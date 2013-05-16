8ffe3a0b5454f57103185128af3c600738eb1a93
R-J <robin.jurinka@gmx.net>
2013-05-16 18:14:01 +0300

    Update class.recentlyactive.plugin.php

--- class.recentlyactive.plugin.php
+++ class.recentlyactive.plugin.php
@@ -1,41 +1,29 @@
 <?php if (!defined('APPLICATION')) exit();
 /**
   * This Plugin includes a module into the side panel.
-  * if all logic could be done in the module, nothing else
-  * than planting the module must be done in the plugin.
-  * So the plugin consists of only three lines
-  * 
-  * 1. include_once(PATH_PLUGINS.DS.'RecentlyActive'.DS.'class.recentlyactivemodule.php');
-  * We're loading the panel module(containing the RecentlyActiveModul
-  * class)
-  *
-  * 2. $RecentlyActiveModule = new RecentlyActiveModule($Sender);
-  * We've included it and so we can now create an instance of it
-  *
-  * 3. $Sender->AddModule($RecentlyActiveModule);
-  * Well, I must admit I haven't understood "$Sender" right now and this is
-  * somehow mysterious to me. It's obvious though, that we have to give the
-  * information on what to do back to our framework  
+  * The module shows the most recently active users with
+  * the time when their last activity has been
   */
 
 $PluginInfo['RecentlyActive'] = array(
    'Name' => 'RecentlyActive',
    'Description' => "Shows the most recently active users in the panel",
-   'Version' => '0.1',
+   'Version' => '1.0',
    'Requires' => FALSE, 
    'HasLocale' => FALSE,
    'Author' => "Robin",
    'RegisterPermissions' => FALSE,
-   'SettingsPermission' => FALSE
+   'SettingsPermission' => FALSE,
+   'AuthorUrl' => 'http://http://vanillaforums.org/discussion/comment/182567/#Comment_182567'
 );
 
 class RecentlyActivePlugin extends Gdn_Plugin {
   
   public function Base_Render_Before($Sender) {
-	// only add the module if we are in the panel asset and NOT in the dashboard
+    // only add the module if we are in the panel asset and NOT in the dashboard
     if(GetValue('Panel',$Sender->Assets) && $Sender->MasterView != 'admin') {
-	  $RecentlyActiveModule = new RecentlyActiveModule($Sender);
+      $RecentlyActiveModule = new RecentlyActiveModule($Sender);
       $Sender->AddModule($RecentlyActiveModule);
-	}
+    }
   }
 }
